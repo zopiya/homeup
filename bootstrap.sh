16c6245b24daf330dd1309c70c8a55e9be4d0dde
zopiya <i@zopiya.com>
2026-01-07 17:19:01 +0800

    fix: update CI workflow to use temporary directory for bootstrap and adjust permissions

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,11 +45,9 @@ if [[ -t 1 ]] && [[ "${TERM:-dumb}" != "dumb" ]]; then
     readonly C_GRAY=$'\033[90m'
     readonly C_RED=$'\033[31m'
     readonly C_CYAN=$'\033[36m'
-    readonly C_YELLOW=$'\033[33m'
-    readonly C_BOLD=$'\033[1m'
 else
     readonly IS_TTY=false
-    readonly C_RESET='' C_GREEN='' C_GRAY='' C_RED='' C_CYAN='' C_YELLOW='' C_BOLD=''
+    readonly C_RESET='' C_GREEN='' C_GRAY='' C_RED='' C_CYAN=''
 fi
 
 # Runtime state (internal)
